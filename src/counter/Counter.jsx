6f0fc5189ed8843ad37a04d4e78a529edec797f0
Ashish Smarttehchnica <ashish.smarttechnica@gmail.com>
2023-06-13 15:42:08 +0530

    pr-5

--- src/counter/Counter.jsx
+++ src/counter/Counter.jsx
@@ -4,12 +4,6 @@ import { useRef } from "react";
 const Counter = () => {
     const count = useRef(1);
 
-  const inc = () => {
-    +count.current.innerHTML++;
-  };
-  const dic = () => {
-    count.current.innerHTML > 1 && +count.current.innerHTML -- ;
-  };
   return (
     <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <a href="#">
@@ -83,7 +77,7 @@ const Counter = () => {
           <button
             type="button"
             className="text-white bg-blue-700 hover:bg-blue-800 font-extrabold rounded-sm text-[20px]  leading-[25px] pb-1 w-[25px] text-center"
-            onClick={inc}
+            onClick={() => +count.current.innerHTML++}
           >
             +
           </button>
@@ -91,7 +85,7 @@ const Counter = () => {
           <button
             type="button"
             className="text-white bg-blue-700 hover:bg-blue-800 font-extrabold rounded-sm text-[20px] w-[25px] text-center"
-            onClick={dic}
+            onClick={() => count.current.innerHTML > 1 && +count.current.innerHTML--}
           >
             -
           </button>
@@ -102,10 +96,7 @@ const Counter = () => {
         <span className="text-3xl font-bold text-gray-900 dark:text-white">
           $599
         </span>
-        <a
-          href="#"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
+        <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" >
           Add to cart
         </a>
       </div>
